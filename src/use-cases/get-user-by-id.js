5f5cfd5a620ff7f06871ab3c8b6618716db2029e
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-10 17:08:10 -0300

    feat: add GetUserByIdUseCase

---
+++ src/use-cases/get-user-by-id.js
@@ -0,0 +1,9 @@
+import { PostgresGetUserByIdRepository } from '../repositories/postgres/get-user-by-id.js'
+
+export class GetUserByIdUseCase {
+    async execute(userId) {
+        const getUserByIdRepository = new PostgresGetUserByIdRepository()
+        const user = await getUserByIdRepository.execute(userId)
+        return user
+    }
+}
