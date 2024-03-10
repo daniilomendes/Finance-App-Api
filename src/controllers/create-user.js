921d7e205f74e4dad02bfd35729041cf3e2f1024
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-10 15:33:28 -0300

    feat: add CreateUserController and validations

---
+++ src/controllers/create-user.js
@@ -0,0 +1,49 @@
+import validator from 'validator'
+import { CreateUserUseCase } from '../use-cases/create-user.js'
+import { badRequest, created, serverError } from './helpers.js'
+
+export class CreateUserController {
+    async execute(httpRequest) {
+        try {
+            const params = httpRequest.body
+
+            // validar a requisição (campos obrigatórios, tamanho de senha e e-mail)
+            const requiredFields = [
+                'first_name',
+                'last_name',
+                'email',
+                'password',
+            ]
+
+            for (const field of requiredFields) {
+                if (!params[field] || params[field].trim().length === 0) {
+                    return badRequest({ message: `Missing param: ${field}.` })
+                }
+            }
+
+            const passwordIsValid = params.password.length < 6
+            if (passwordIsValid) {
+                return badRequest({
+                    message: 'Password must be at least 6 characters.',
+                })
+            }
+
+            const emailIsValid = validator.isEmail(params.email)
+            if (!emailIsValid) {
+                return badRequest({
+                    message: 'Invalid e-mail. Please provide a valid one.',
+                })
+            }
+
+            // chamar o use case
+            const createUserUseCase = new CreateUserUseCase()
+            const createdUser = await createUserUseCase.execute(params)
+
+            // retornar a resposta para o usuário (status code)
+            return created(createdUser)
+        } catch (error) {
+            console.error(error)
+            return serverError()
+        }
+    }
+}
