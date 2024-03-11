3e71d759cf00f3942feb852153ed769a2814515b
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-11 05:41:01 -0300

    feat: ensure EmailAlreadyInUseError is used

--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -2,6 +2,7 @@ import { v4 as uuidv4 } from 'uuid'
 import bcrypt from 'bcrypt'
 import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js'
 import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js'
+import { EmailAlreadyInUseError } from '../errors/user.js'
 
 export class CreateUserUseCase {
     async execute(createUserParams) {
@@ -15,7 +16,7 @@ export class CreateUserUseCase {
             )
 
         if (userWithProvidedEmail) {
-            throw new Error('The provider e-mail is already in use.')
+            throw new EmailAlreadyInUseError(createUserParams.email)
         }
 
         // gerar ID do usuário
