fdea555729cd19b8a8cc0c2833eb46240a96a286
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-13 05:13:58 -0300

    feat: add dependency injection to get user by id route

--- src/controllers/get-user-by-id.js
+++ src/controllers/get-user-by-id.js
@@ -1,4 +1,3 @@
-import { GetUserByIdUseCase } from '../use-cases/index.js'
 import {
     ok,
     serverError,
@@ -8,6 +7,10 @@ import {
 } from './helpers/index.js'
 
 export class GetUserByIdController {
+    constructor(getUserByIdUseCase) {
+        this.getUserByIdUseCase = getUserByIdUseCase
+    }
+
     async execute(httpRequest) {
         try {
             const isIdValid = checkIfIdIsValid(httpRequest.params.userId)
@@ -15,8 +18,7 @@ export class GetUserByIdController {
                 return invalidIdResponse()
             }
 
-            const getUserByIdUseCase = new GetUserByIdUseCase()
-            const user = await getUserByIdUseCase.execute(
+            const user = await this.getUserByIdUseCase.execute(
                 httpRequest.params.userId,
             )
 
--- src/use-cases/get-user-by-id.js
+++ src/use-cases/get-user-by-id.js
@@ -1,9 +1,11 @@
-import { PostgresGetUserByIdRepository } from '../repositories/postgres/index.js'
-
 export class GetUserByIdUseCase {
+    constructor(getUserByIdRepository) {
+        this.getUserByIdRepository = getUserByIdRepository
+    }
+
     async execute(userId) {
-        const getUserByIdRepository = new PostgresGetUserByIdRepository()
-        const user = await getUserByIdRepository.execute(userId)
+        const user = await this.getUserByIdRepository.execute(userId)
+
         return user
     }
 }
