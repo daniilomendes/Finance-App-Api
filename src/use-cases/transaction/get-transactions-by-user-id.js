df0a076b35e0a658538b2ef226ad6ec6d07bb2d6
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-14 18:40:14 -0300

    fix: ensure GetTransactionsByUserId throws UserNotFoundError

--- src/use-cases/transaction/get-transactions-by-user-id.js
+++ src/use-cases/transaction/get-transactions-by-user-id.js
@@ -1,4 +1,4 @@
-import { userNotFoundResponse } from '../../controllers/helpers.js'
+import { UserNotFoundError } from '../../errors/user.js'
 
 export class GetTransactionsByUserId {
     constructor(getTransactionsByUserIdRepository, getUserByIdRepository) {
@@ -12,7 +12,7 @@ export class GetTransactionsByUserId {
         const user = await this.getUserByIdRepository.execute(params.userId)
 
         if (!user) {
-            return userNotFoundResponse()
+            throw new UserNotFoundError(params.userId)
         }
 
         // chamar o repository
