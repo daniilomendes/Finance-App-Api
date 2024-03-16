300cd1aa5b1f0ae36d6a6fac288337516e11eddc
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-16 09:12:23 -0300

    feat: add GetUserBalanceUseCase

--- src/use-cases/index.js
+++ src/use-cases/index.js
@@ -1,9 +1,14 @@
+// USER
 export * from './user/create-user.js'
 export * from './user/get-user-by-id.js'
 export * from './user/update-user.js'
 export * from './user/delete-user.js'
 
+// TRANSACTION
 export * from './transaction/create-transaction.js'
 export * from './transaction/get-transactions-by-user-id.js'
 export * from './transaction/update-transaction.js'
 export * from './transaction/delete-transaction.js'
+
+// BALANCE
+export * from './user/get-user-balance.js'
