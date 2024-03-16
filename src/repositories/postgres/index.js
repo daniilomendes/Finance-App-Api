8be9caeed686cfd8db6238287b2bb29fbd23e31b
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-15 21:52:54 -0300

    feat: add PostgresGetUserBalanceRepository

--- src/repositories/postgres/index.js
+++ src/repositories/postgres/index.js
@@ -1,10 +1,15 @@
+// USER
 export * from './user/create-user.js'
 export * from './user/get-user-by-email.js'
 export * from './user/get-user-by-id.js'
 export * from './user/update-user.js'
 export * from './user/delete-user.js'
 
+// TRANSACTION
 export * from './transaction/create-transaction.js'
 export * from './transaction/get-transactions-by-user-id.js'
 export * from './transaction/update-transaction.js'
 export * from './transaction/delete-transaction.js'
+
+// BALANCE
+export * from './user/get-user-balance.js'
---
+++ src/repositories/postgres/user/get-user-balance.js
@@ -0,0 +1,27 @@
+import { PostgresHelper } from '../../../db/postgres/helper.js'
+
+export class PostgresGetUserBalanceRepository {
+    async execute(userId) {
+        const balance = await PostgresHelper.query(
+            `
+    	    SELECT
+                SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END) AS investiments,
+                SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END) AS expense,
+                SUM(CASE WHEN type = 'EARNING' THEN amount ELSE 0 END) AS eranings,
+                (
+                    SUM(CASE WHEN type = 'EARNING' THEN amount ELSE 0 END)
+                    - SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END)
+                    - SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END)
+                ) AS balance
+            FROM transactions
+            WHERE user_id = $1;
+            `,
+            [userId],
+        )
+
+        return {
+            userId,
+            ...balance[0],
+        }
+    }
+}
