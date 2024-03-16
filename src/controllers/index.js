f8bc416a0338166888206cdcf5b3dabcce93997f
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-16 09:21:01 -0300

    feat: add GetUserBalanceController

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,14 @@
+// USER
 export * from './user/create-user.js'
 export * from './user/get-user-by-id.js'
 export * from './user/update-user.js'
 export * from './user/delete-user.js'
 
+// TRANSACTION
 export * from './transactions/create-transaction.js'
 export * from './transactions/get-transactions-by-user-id.js'
 export * from './transactions/update-transaction.js'
 export * from './transactions/delete-transaction.js'
+
+// BALANCE
+export * from './user/get-user-balance.js'
---
+++ src/controllers/user/get-user-balance.js
@@ -0,0 +1,37 @@
+import { UserNotFoundError } from '../../errors/user.js'
+import {
+    checkIfIdIsValid,
+    invalidIdResponse,
+    ok,
+    serverError,
+    userNotFoundResponse,
+} from '../helpers/index.js'
+
+export class GetUserBalanceController {
+    constructor(getUserBalanceUseCase) {
+        this.getUserBalanceUseCase = getUserBalanceUseCase
+    }
+
+    async execute(httpRequest) {
+        try {
+            const userId = httpRequest.params.userId
+
+            const idIsValid = checkIfIdIsValid()
+
+            if (!idIsValid) {
+                return invalidIdResponse()
+            }
+
+            const balance = await this.getUserBalanceUseCase.execute({ userId })
+
+            return ok(balance)
+        } catch (error) {
+            if (error instanceof UserNotFoundError) {
+                return userNotFoundResponse
+            }
+
+            console.error(error)
+            return serverError()
+        }
+    }
+}
