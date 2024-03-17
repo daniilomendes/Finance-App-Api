eb1d3eb7b2095026603f092e9e85c29158194dd2
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-17 10:33:34 -0300

    test: ensure 201 is returned when user is created

---
+++ src/controllers/user/create-user.test.js
@@ -0,0 +1,34 @@
+import { CreateUserController } from './create-user'
+
+describe('Create User Controller', () => {
+    class CreateUserUseCaseStub {
+        execute(user) {
+            return user
+        }
+    }
+
+    it('should return 201 when creating a user successfully', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: 'Danilo',
+                last_name: 'Mendes',
+                email: 'dan@gmail.com',
+                password: '123456789',
+            },
+        }
+
+        // act
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(201)
+        expect(result.body).not.toBeUndefined()
+        expect(result.body).not.toBeNull()
+        expect(result.body.first_name).toBe('Danilo')
+        expect(result.body).toBe(httpRequest.body)
+    })
+})
