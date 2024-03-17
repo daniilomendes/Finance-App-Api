332d70c2b73b0125f50c7fc1edc68a1bc0f8a3d7
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-17 19:57:57 -0300

    test: ensure GetTransactionsByUserId return 200 when finding transaction by user id successfully

---
+++ src/controllers/transactions/get-transactions-by-user-id.test.js
@@ -0,0 +1,40 @@
+import { faker } from '@faker-js/faker'
+import { GetTransactionsByUserIdController } from './get-transactions-by-user-id.js'
+
+describe('Get Transactions By User Id', () => {
+    class GetTransactionsByUserIdUseCaseStub {
+        async execute() {
+            return {
+                user_id: faker.string.uuid(),
+                id: faker.string.uuid(),
+                name: faker.commerce.productName(),
+                date: faker.date.anytime().toISOString(),
+                type: 'EXPENSE',
+                amount: Number(faker.finance.amount()),
+            }
+        }
+    }
+
+    const makeSut = () => {
+        const getTransactionsByUserIdUseCase =
+            new GetTransactionsByUserIdUseCaseStub()
+        const sut = new GetTransactionsByUserIdController(
+            getTransactionsByUserIdUseCase,
+        )
+
+        return { sut, getTransactionsByUserIdUseCase }
+    }
+
+    it('should return 200 when finding transaction by user id successfully', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const response = await sut.execute({
+            query: { userId: faker.string.uuid() },
+        })
+
+        // assert
+        expect(response.statusCode).toBe(200)
+    })
+})
