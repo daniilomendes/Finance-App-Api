3ada9f63d273b2fe67d1af1467c66be13705cb5e
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-17 19:46:36 -0300

    test: ensure DeleteTransactionController return 404 if when transaction is not found

--- src/controllers/transactions/delete-transaction.test.js
+++ src/controllers/transactions/delete-transaction.test.js
@@ -47,4 +47,20 @@ describe('Delete Transaction Controller', () => {
         // assert
         expect(response.statusCode).toBe(400)
     })
+
+    it('should return 404 if when transaction is not found', async () => {
+        // arrange
+        const { sut, deleteTransactionUseCase } = makeSut()
+        jest.spyOn(deleteTransactionUseCase, 'execute').mockResolvedValueOnce(
+            null,
+        )
+
+        // act
+        const response = await sut.execute({
+            params: { transactionId: faker.string.uuid() },
+        })
+
+        // assert
+        expect(response.statusCode).toBe(404)
+    })
 })
