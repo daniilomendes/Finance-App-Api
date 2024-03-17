1605346f21170ef5c256ed9b61218f888c4fc3db
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-17 19:17:21 -0300

    test: ensure CreateTransactionController returns 400 when type is not EXPENSE, EARNING or INVESTMENT

--- src/controllers/transactions/create-transaction.test.js
+++ src/controllers/transactions/create-transaction.test.js
@@ -133,4 +133,20 @@ describe('Create Transaction Controller', () => {
         // assert
         expect(response.statusCode).toBe(400)
     })
+
+    it('should return 400 when type is not EXPENSE, EARNING or INVESTMENT', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const response = await sut.execute({
+            body: {
+                ...httpRequest.body,
+                type: 'invalid_type',
+            },
+        })
+
+        // assert
+        expect(response.statusCode).toBe(400)
+    })
 })
