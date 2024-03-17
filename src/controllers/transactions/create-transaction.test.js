5133a343e4d0fd6174e0d1679ef473e3bedb94b3
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-17 19:23:43 -0300

    test: ensure CreateTransactionController returns 400 when amount is not valid currency

--- src/controllers/transactions/create-transaction.test.js
+++ src/controllers/transactions/create-transaction.test.js
@@ -181,4 +181,20 @@ describe('Create Transaction Controller', () => {
         // assert
         expect(response.statusCode).toBe(400)
     })
+
+    it('should return 400 when amount is not valid currency', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const response = await sut.execute({
+            body: {
+                ...httpRequest.body,
+                amount: 'invalid_amount',
+            },
+        })
+
+        // assert
+        expect(response.statusCode).toBe(400)
+    })
 })
