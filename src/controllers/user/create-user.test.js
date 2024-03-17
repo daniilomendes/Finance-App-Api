9957cbbf934d4b58c24a8023c722042dcadff592
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-17 11:00:02 -0300

    test: ensure 400 is returned if email is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -71,4 +71,24 @@ describe('Create User Controller', () => {
         // assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if email is not provided', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: 'Danilo',
+                last_name: 'Mendes',
+                password: '123456789',
+            },
+        }
+
+        // act
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
 })
