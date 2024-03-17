33decdab8447b610ae6e2c361a0389a3aa5e2a9d
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-17 20:00:55 -0300

    test: ensure GetTransactionsByUserId return  400 when userId param is invalid

--- src/controllers/transactions/get-transactions-by-user-id.test.js
+++ src/controllers/transactions/get-transactions-by-user-id.test.js
@@ -50,4 +50,17 @@ describe('Get Transactions By User Id', () => {
         // assert
         expect(response.statusCode).toBe(400)
     })
+
+    it('should return 400 when userId param is invalid', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const response = await sut.execute({
+            query: { userId: 'invalid_user_id' },
+        })
+
+        // assert
+        expect(response.statusCode).toBe(400)
+    })
 })
