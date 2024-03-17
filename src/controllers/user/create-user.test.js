9dd1732dfcfe7b5042e762213c7a533d10e9f4d6
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-17 11:05:36 -0300

    test: ensure 400 is returned if password is not provided or is less than 6 characters

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -112,4 +112,45 @@ describe('Create User Controller', () => {
         // assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if password is not provided', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: 'Danilo',
+                last_name: 'Mendes',
+                email: 'dan@gmail.com',
+            },
+        }
+
+        // act
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
+
+    it('should return 400 if password is less than 6 characters', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: 'Danilo',
+                last_name: 'Mendes',
+                email: 'dan@gmail.com',
+                password: '123',
+            },
+        }
+
+        // act
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
 })
