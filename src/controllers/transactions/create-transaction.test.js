35b5fac16fee516448129d37fbbba5a8b1a417a7
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-18 11:26:33 -0300

    test: ensure DeleteTransactionController calls  CreateTransactionUseCase with correct params

--- src/controllers/transactions/create-transaction.test.js
+++ src/controllers/transactions/create-transaction.test.js
@@ -212,4 +212,16 @@ describe('Create Transaction Controller', () => {
         // assert
         expect(response.statusCode).toBe(500)
     })
+
+    it('should call CreateTransactionUseCase with correct params', async () => {
+        // arrange
+        const { sut, createTransactionUseCaseStub } = makeSut()
+        const executeSpy = jest.spyOn(createTransactionUseCaseStub, 'execute')
+
+        // act
+        await sut.execute(httpRequest)
+
+        // assert
+        expect(executeSpy).toHaveBeenCalledWith(httpRequest.body)
+    })
 })
