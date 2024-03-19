814117b117f587d49bc571bf0d0d969c9d943c40
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-19 11:53:58 -0300

    refactor: remove unused code

--- src/controllers/helpers/validation.js
+++ src/controllers/helpers/validation.js
@@ -13,28 +13,3 @@ export const requiredFieldIsMissingResponse = (field) => {
 }
 
 export const checkIfIdIsValid = (id) => validator.isUUID(id)
-
-export const checkIfIsString = (value) => typeof value === 'string'
-
-export const validateRequiredFields = (params, requiredFields) => {
-    for (const field of requiredFields) {
-        const fieldIsMissing = !params[field]
-        const fieldIsEmpty =
-            checkIfIsString(params[field]) &&
-            validator.isEmpty(params[field], {
-                ignore_whitespace: true,
-            })
-
-        if (fieldIsMissing || fieldIsEmpty) {
-            return {
-                missingField: field,
-                ok: false,
-            }
-        }
-    }
-
-    return {
-        ok: true,
-        missingField: undefined,
-    }
-}
